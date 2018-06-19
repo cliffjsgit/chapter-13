c991298ac0d2d190a2a10e7ab855c8404939e25e
Philip Ulrich <philip@ulrich.life>
2018-06-19 21:32:21 +0000

    adding grader

---
+++ grade.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+import shelve
+import urllib.request
+
+chapter = "13"
+exercises = ['13.1','13.2','13.3','13.4','13.5']
+loadedList = []
+db = shelve.open('chapter13.db', flag='c', writeback=True)
+db['loaded'] = {}
+if 'submitted' not in db:
+    db['submitted'] = {}
+
+try:
+    import exercise131
+    db['loaded']['13.1'] = True
+except:
+    db['loaded']['13.1'] = False
+try:
+    import exercise132
+    db['loaded']['13.2'] = True
+except:
+    db['loaded']['13.2'] = False
+try:
+    import exercise133
+    db['loaded']['13.3'] = True
+except:
+    db['loaded']['13.3'] = False
+try:
+    import exercise134
+    db['loaded']['13.4'] = True
+except:
+    db['loaded']['13.4'] = False
+try:
+    import exercise135
+    db['loaded']['13.5'] = True
+except:
+    db['loaded']['13.5'] = False
+
+db.sync()
+
+def menu():
+    while True:
+        for exercise in loadedList:
+            if exercise in db['submitted']:
+                print('[x] Exercise ' + exercise)
+            else:
+                print('[ ] Exercise ' + exercise)
+        print('    Enter q to exit')
+        str_in = input('Exercise (e.g. 13.1): ')
+        if str_in in loadedList:
+            grade(str_in)
+            break
+        elif str_in.lower() == 'q':
+            break
+        else:
+            print('Incorrect response. Only enter the exercise number. Example: "13.1" (no quotes).')
+            
+def grade(assignment):
+    if assignment == '13.1':
+        exercise131.strip_and_lower("emma.txt")
+        if True:
+            db['submitted'][assignment] = True
+            submit('exercise131.py',exercise131.__author__)
+            str_in = input("Exercise answer correct and submitted. Would you like to submit another? (y/n): ")
+            if str_in.lower() == 'y':
+                menu()
+        else:
+            str_in = input('The exercise answer was incorrect. Did you still want to submit it? (y/n): ')
+            if str_in.lower() == 'y':
+                db['submitted'][assignment] = True
+                submitbad('exercise131.py',exercise131.__author__)
+                str_in = input("Exercise submitted. Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+            else:
+                print('Assignment was not submitted')
+                str_in = input("Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+    elif assignment == '13.2':
+        if 'volume' in exercise132.strip_and_lower("emma.txt"):
+            db['submitted'][assignment] = True
+            submit('exercise132.py',exercise132.__author__)
+            str_in = input("Exercise answer correct and submitted. Would you like to submit another? (y/n): ")
+            if str_in.lower() == 'y':
+                menu()
+        else:
+            str_in = input('The exercise answer was incorrect. Did you still want to submit it? (y/n): ')
+            if str_in.lower() == 'y':
+                db['submitted'][assignment] = True
+                submitbad('exercise132.py',exercise132.__author__)
+                str_in = input("Exercise submitted. Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+            else:
+                print('Assignment was not submitted')
+                str_in = input("Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+    elif assignment == '13.3':
+        if exercise133.strip_and_lower("emma.txt")[0] == 'to':
+            db['submitted'][assignment] = True
+            submit('exercise133.py',exercise133.__author__)
+            str_in = input("Exercise answer correct and submitted. Would you like to submit another? (y/n): ")
+            if str_in.lower() == 'y':
+                menu()
+        else:
+            str_in = input('The exercise answer was incorrect. Did you still want to submit it? (y/n): ')
+            if str_in.lower() == 'y':
+                db['submitted'][assignment] = True
+                submitbad('exercise133.py',exercise133.__author__)
+                str_in = input("Exercise submitted. Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+            else:
+                print('Assignment was not submitted')
+                str_in = input("Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+    elif assignment == '13.4':
+        if 'excusable' in exercise134.strip_and_lower("emma.txt"):
+            db['submitted'][assignment] = True
+            submit('exercise134.py',exercise134.__author__)
+            str_in = input("Exercise answer correct and submitted. Would you like to submit another? (y/n): ")
+            if str_in.lower() == 'y':
+                menu()
+        else:
+            str_in = input('The exercise answer was incorrect. Did you still want to submit it? (y/n): ')
+            if str_in.lower() == 'y':
+                db['submitted'][assignment] = True
+                submitbad('exercise134.py',exercise134.__author__)
+                str_in = input("Exercise submitted. Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+            else:
+                print('Assignment was not submitted')
+                str_in = input("Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+    elif assignment == '13.5':
+        exercise135.choose_from_hist("emma.txt")
+        if True:
+            db['submitted'][assignment] = True
+            submit('exercise134.py',exercise134.__author__)
+            str_in = input("Exercise answer correct and submitted. Would you like to submit another? (y/n): ")
+            if str_in.lower() == 'y':
+                menu()
+        else:
+            str_in = input('The exercise answer was incorrect. Did you still want to submit it? (y/n): ')
+            if str_in.lower() == 'y':
+                db['submitted'][assignment] = True
+                submitbad('exercise134.py',exercise134.__author__)
+                str_in = input("Exercise submitted. Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+            else:
+                print('Assignment was not submitted')
+                str_in = input("Would you like to submit another? (y/n): ")
+                if str_in.lower() == 'y':
+                    menu()
+    else:
+        print('This should not have happened. Let your instructor know.')
+    db.sync()
+            
+def submit(file,name):
+    with open(file,'rb') as fin:
+        assignment = fin.read()
+        url = 'https://1402-answer-repo.s3.amazonaws.com/assignments/'+name+'/'+chapter+'/'+file
+        req = urllib.request.Request(url.replace(' ',''), data=assignment, method='PUT')
+        urllib.request.urlopen(req)
+
+def submitbad(file,name):
+    with open(file,'rb') as fin:
+        assignment = fin.read()
+        url = 'https://1402-answer-repo.s3.amazonaws.com/assignments/'+name+'/'+chapter+'/incorrect/'+file
+        req = urllib.request.Request(url.replace(' ',''), data=assignment, method='PUT')
+        urllib.request.urlopen(req)
+            
+def main():
+    for exercise in exercises:
+        if db['loaded'][exercise]:
+            loadedList.append(exercise)
+    
+    print('The following exercises have been loaded: ' + ', '.join(loadedList) + '. Which would you like to grade?')
+    menu()
+
+
+main()
+
+db.close()
